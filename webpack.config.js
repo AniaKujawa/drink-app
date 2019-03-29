a9a611f980fff77d03b11157db80824e6ebffd5c
Ania Kujawa <kujawanna@gmail.com>
2019-03-29 14:45:21 +0100

    rwd

--- webpack.config.js
+++ webpack.config.js
@@ -65,6 +65,7 @@ module.exports = {
       hash: true,
       filename: 'index.html',
       template: './src/index.html',
+      meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'}
     }),
     new MiniCSSExtractPlugin(),
   ]
