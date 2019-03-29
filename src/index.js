9349639d71e98e29c34ff467ad97f41912189a1f
Ania Kujawa <kujawanna@gmail.com>
2019-03-29 13:24:45 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './styles/main.scss';
+import './styles/assets/bijou.jpg';
+import './styles/assets/eggnogg.jpg';
+import './styles/assets/mojito.jpg';
+import './styles/assets/negroni.jpg';
+import './styles/assets/rattlesnake.jpg';
+import './styles/assets/zinger.jpg';
+
+
---
+++ webpack.config.prod.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+const distDir = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: distDir
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        use: [
+          {loader:'style-loader', options: {sourceMap: true}},
+          {loader: 'css-loader', options: {sourceMap: true}},
+          {loader: 'postcss-loader', options: {sourceMap: true}},
+          {loader:'sass-loader', options: {sourceMap: true}}
+         ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+            }
+        }]
+    }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin( {
+      hash: true,
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new MiniCSSExtractPlugin(),
+  ]
+};
